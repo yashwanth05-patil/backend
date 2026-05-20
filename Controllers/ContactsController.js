dd3725e72a1543a14b034f7fa6076f40a8c0e87c
yashwanth05-patil <yashwanth0527@gmail.com>
2026-05-20 12:13:37 +0530

    fix: force IPv4 for Gmail SMTP to fix ENETUNREACH on Render

--- Controllers/ContactsController.js
+++ Controllers/ContactsController.js
@@ -20,6 +20,7 @@ const transporter = nodemailer.createTransport({
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
+  family: 4,
 });
 
 const AddContact = async (req, res) => {
