475b910474772895566c929c50db69776b8bf9cb
yashwanth05-patil <yashwanth0527@gmail.com>
2026-05-20 12:00:23 +0530

    fix: switch nodemailer to port 587 to fix ENETUNREACH on Render

--- Controllers/ContactsController.js
+++ Controllers/ContactsController.js
@@ -7,10 +7,15 @@ import axios from "axios";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
   connectionTimeout: 10000,
   greetingTimeout: 10000,
